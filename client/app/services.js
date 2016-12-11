619ac334a5649feaca0d113dee5bcf525d3bd48e
Nick Barry <niko.barry@gmail.com>
2016-12-10 23:08:25 -0800

    feat: Add generic API call functions to the API service

--- client/app/services.js
+++ client/app/services.js
@@ -1,5 +1,42 @@
 angular.module('app.services', [])
 .factory('API', function($http) {
+  const getAll = () => {
+    return $http({
+      method: 'GET',
+      url: '/api/data',
+    }).then(response => response.data);
+  };
 
-  return {};
+  const getOne = (query) => {
+    const queryString = Object.keys(query)
+      .map(key => `${key}=${query[key]}`)
+      .join('&');
+    return $http({
+      method: 'GET',
+      url: `/api/data/?${queryString}`,
+    }).then(response => response.data);
+  };
+
+  const addOne = (data) => {
+    return $http({
+      method: 'POST',
+      url: '/api/data',
+      data: data,
+    });
+  };
+
+  const updateOne = (id, latestData) => {
+    return $http({
+      method: 'POST',
+      url: `/api/data/${id}`,
+      data: latestData,
+    });
+  };
+
+  const deleteOne = (id) => $http({
+    method: 'DELETE',
+    url: `/api/data/${id}`,
+  });
+
+  return { getAll, getOne, addOne, updateOne, deleteOne };
 });
