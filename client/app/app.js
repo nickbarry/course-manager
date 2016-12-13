c9c854706c320f03e4a89960fc84634457c1c41a
Nick Barry <niko.barry@gmail.com>
2016-12-13 11:15:55 -0800

    feat: Display course/student interesections in the table
    Each table cell now displays a 'Y' if the student is enrolled in that course.

--- client/app/app.js
+++ client/app/app.js
@@ -3,9 +3,15 @@ angular.module('app', ['app.services'])
   $scope.message = 'Student Enrollment';
   $scope.courses = [];
   $scope.students = [];
+  $scope.courses_students = [];
+
+  $scope.studentIsInCourse = (student, course) => (
+    $scope.courses_students
+      .filter(row => row.student_id === student.id && row.course_id === course.id )
+      .length >= 1
+  );
 
   API.getAll().then(data => {
-    $scope.courses = data.courses;
-    $scope.students = data.students;
+    Object.assign($scope, data);
   });
 });
